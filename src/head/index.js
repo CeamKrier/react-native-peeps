1866c5bf66449b491fa4bf9288ff5bc16a93e8fc
Emre Çakır <ceamkrier@gmail.com>
2020-03-03 21:05:00 +0300

    feat: available hairs have been implemented

--- src/head/index.js
+++ src/head/index.js
@@ -4,13 +4,14 @@ import { G } from 'react-native-svg'
 
 import { VeryAngry } from '../face/VeryAngry'
 import { LongAfro } from '../hair/LongAfro'
+import { MohawkDino } from '../hair/MohawkDino'
 
 const index = () => {
   return (
 
     <G id='Head' transform='translate(225 0)'>
       <G id='HEAD'>
-        <LongAfro />
+        <MohawkDino />
       </G>
       <G id='FACE' transform='translate(159 186)'>
         <VeryAngry />
